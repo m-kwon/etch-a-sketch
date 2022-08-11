0c847c67a7f6a41fa7d455b3f150dacd010e6d11
Matthew Kwon <matthew.o.kwon@gmail.com>
2022-08-11 11:17:52 -0700

    Added grid size button with prompt

--- app.js
+++ app.js
@@ -11,3 +11,12 @@ for (let i = 1; i <= 256; i++) {
 }
 
 body.appendChild(grid);
+
+// BUTTON
+const gridButton = document.createElement('button');
+gridButton.classList.add('grid-button');
+gridButton.innerHTML = 'Change Grid Size';
+gridButton.type = 'button';
+gridButton.onclick = () => prompt('Enter new grid size');
+
+body.appendChild(gridButton);
